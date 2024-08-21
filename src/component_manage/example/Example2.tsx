9750ee1d94753f3fc77ee174a5a96e38ab0c63e9
LYH <dudgk7829@naver.com>
2024-08-21 09:53:53 +0900

    20240821

--- src/component_manage/example/Example2.tsx
+++ src/component_manage/example/Example2.tsx
@@ -1,33 +1,239 @@
+// 간단한 방법 1
+
+// import React from 'react'
+// import './Example2.css';
+
+
+// function TableHeader() {
+
+//     return (
+//         <div className='th'>
+//             <div className='td'>사번</div>
+//             <div className='td'>이름</div>
+//             <div className='td'>출근상태</div>
+//         </div>
+//     );
+    
+// }
+
+// interface TableRowProps {
+//     employeeNumber: string;
+//     employeeName: string;
+//     workState: Boolean;
+// }
+
+// function TableRow({ employeeNumber, employeeName, workState }: TableRowProps) {
+
+//     const color = workState ? 'blue' : 'red';
+
+//     return(
+
+//         <div className='tr'>
+//             <div className='td'>{employeeNumber}</div>
+//             <div className='td'>{employeeName}</div>
+//             <div className='td' style={{ color }}>●</div>
+//         </div>
+//     );
+    
+// }
+
+// export default function Example2() {
+//     return (
+//         <div className='table'>
+
+//             <TableHeader />
+//             <TableRow employeeNumber='202401' employeeName='홍길동' workState={false} />
+//             <TableRow employeeNumber='202402' employeeName='이영희' workState />
+//             <TableRow employeeNumber='202403' employeeName='김철수' workState />
+
+//         </div>
+//     )
+// }
+
+// -------------------------------------------------------------------------------------------
+// 간단한 방법 2
+
+// import React from 'react'
+// import './Example2.css';
+
+
+// function TableHeader() {
+
+//     return (
+//         <div className='th'>
+//             <div className='td'>사번</div>
+//             <div className='td'>이름</div>
+//             <div className='td'>출근상태</div>
+//         </div>
+//     );
+    
+// }
+
+// interface TableRowProps {
+//     employeeNumber: string;
+//     employeeName: string;
+//     workState: Boolean;
+// }
+
+// function TableRow({ employeeNumber, employeeName, workState }: TableRowProps) {
+
+//     const color = workState ? 'blue' : 'red';
+
+//     return(
+
+//         <div className='tr'>
+//             <div className='td'>{employeeNumber}</div>
+//             <div className='td'>{employeeName}</div>
+//             <div className='td' style={{ color }}>●</div>
+//         </div>
+
+//     );
+    
+// }
+
+// export default function Example2() {
+
+//     const employee1 = {
+//         employeeNumber: '202401',
+//         employeeName: '홍길동',
+//         workState: false
+//     }
+
+//     const employee2 = {
+//         employeeNumber: '202402',
+//         employeeName: '이영희',
+//         workState: true
+//     }
+
+//     const employee3 = {
+//         employeeNumber: '202403',
+//         employeeName: '김철수',
+//         workState: true
+//     }
+
+//     return (
+//         <div className='table'>
+
+//             <TableHeader />
+//             <TableRow { ...employee1} />
+//             <TableRow { ...employee2} />
+//             <TableRow { ...employee3} />
+
+//         </div>
+//     )
+// }
+
+// -------------------------------------------------------------------------------------------
+// 복잡하지만 임포넌트 한줄 방법
+
 import React from 'react'
 import './Example2.css';
 
 
-export default function Example2() {
+function TableHeader() {
+
     return (
-        <div className='table'>
+        <div className='th'>
+            <div className='td'>사번</div>
+            <div className='td'>이름</div>
+            <div className='td'>출근상태</div>
+        </div>
+    );
+    
+}
+
+interface TableRowProps {
+    employeeNumber: string;
+    employeeName: string;
+    workState: Boolean;
+}
+
+function TableRow({ employeeNumber, employeeName, workState }: TableRowProps) {
+
+    const color = workState ? 'blue' : 'red';
+
+    return(
+
+        <div className='tr'>
+            <div className='td'>{employeeNumber}</div>
+            <div className='td'>{employeeName}</div>
+            <div className='td' style={{ color }}>●</div>
+        </div>
+
+    );
+    
+}
+
+
+interface TableDataTrProps {
+
+    type: 'tr';
+    employeeNumber: string;
+    employeeName: string;
+    workState: Boolean;
+
+}
+
+interface TableDataThProps {
+    type: 'th'
+}
+
+function TableData(props: TableDataTrProps | TableDataThProps) {
+
+    const { type } = props;
+    if (type === 'th') {
+        return (
             <div className='th'>
                 <div className='td'>사번</div>
                 <div className='td'>이름</div>
                 <div className='td'>출근상태</div>
             </div>
-            
-            <div className='tr'>
-                <div className='td'>202401</div>
-                <div className='td'>홍길동</div>
-                <div className='td' style={{color:'red'}}>●</div>
-            </div>
-            
-            <div className='tr'>
-                <div className='td'>202402</div>
-                <div className='td'>이영희</div>
-                <div className='td' style={{color:'blue'}}>●</div>
-            </div>
-            
-            <div className='tr'>
-                <div className='td'>202403</div>
-                <div className='td'>김철수</div>
-                <div className='td'style={{color:'blue'}}>●</div>
-            </div>
+        );
+    }
+
+    const { employeeNumber, employeeName, workState } = props;
+    const color = workState ? 'blue' : 'red';
+
+    return(
+
+        <div className='tr'>
+            <div className='td'>{employeeNumber}</div>
+            <div className='td'>{employeeName}</div>
+            <div className='td' style={{ color }}>●</div>
+        </div>
+
+    );
+
+}
+
+export default function Example2() {
+
+    const employee1 = {
+        employeeNumber: '202401',
+        employeeName: '홍길동',
+        workState: false
+    }
+
+    const employee2 = {
+        employeeNumber: '202402',
+        employeeName: '이영희',
+        workState: true
+    }
+
+    const employee3 = {
+        employeeNumber: '202403',
+        employeeName: '김철수',
+        workState: true
+    }
+
+    return (
+        <div className='table'>
+
+            <TableHeader />
+            <TableRow { ...employee1} />
+            <TableRow { ...employee2} />
+            <TableRow { ...employee3} />
+
         </div>
     )
 }
