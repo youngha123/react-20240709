42e38d6e6ca23d31c6bed36dacf6d82e7add4395
LYH <dudgk7829@naver.com>
2024-08-23 09:54:14 +0900

    20240823 09:50

--- src/interaction/StateComponent.tsx
+++ src/interaction/StateComponent.tsx
@@ -13,21 +13,76 @@ export default function StateComponent() {
 
     // let count: number = 0;
     const [count, setCount] = useState<number>(0);
-
+    const [total, setTotal] = useState<number>(0);
     // let counts: number[] = [];
     const [counts, setCounts] = useState<number[]>([]);
 
     const onIncrease = () => {
-        setCount(count + 1);
+        // setCount(count + 1);
 
         // 상태 변수는 반드시 상태변경함수를 통해서 변경해야 리렌더링 됨
         // count++;
         // console.log(count);
+
+        // 상태변경함수를 사용하여 상태를 변경한다고 바로 변경되지 않음
+        // 함수가 끝나고 렌더링 되는 시점에 적용됨
+        // 0
+        // setCount(count + 1);
+        // 0
+        // setCount(count + 2);
+        // 0
+        // setCount(count + 3);
+
+        // 상태변경함수에 콜백함수를 전달하면 해당 콜백 함수는 누적되어 실행됨
+
+        // 상태 count : 0
+        // argCount : 0
+        // console.log('상태 count : ' + count);
+        // setCount(count => {
+        //     console.log('count : ' + count);
+        //     return count + 1;
+        // });
+
+        // 상태 count : 0
+        // argCount : 1
+        // console.log('상태 count : ' + count);
+        // setCount(count => {
+        //     console.log('count : ' + count);
+        //     return count + 2;
+        // });
+
+        // 상태 count : 0
+        // argCount : 3
+        // console.log('상태 count : ' + count);
+        // setCount(count => {
+        //     console.log('count : ' + count);
+        //     return count + 3;
+        // });
+        
+        // 상태 변경하는 작업이 한번에 여러번 실행된다면 임시변수 활용하여 사용할 수 있음
+
+        // let tmp = 0;
+        // tmp = count;
+        // tmp += 1;
+        // tmp += 2;
+        // tmp += 3;
+        // setCount(tmp);
+        
+        // 아래 코드는 setTotal에서 기존의 count 값을 사용하기 때문에 동작이 한 단계 전 단계로 동작함
+        // setCount(count + 1);
+        // setTotal(total + count);
+
+        // 임시변수를 사용하여 변경 결과 값을 미리 저장하고 사용하면 위 코드의 문제를 해결할 수 있음
+        const newCount = count + 1;
+        setCount(count + 1);
+        setTotal(total + count);
+
     };
 
     return (
         <div>
             <h1>{count}</h1>
+            <h1>total : {total}</h1>
             <button onClick={onIncrease}>+</button>
         </div>
     )
